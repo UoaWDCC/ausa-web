ddc7a8ee625748ce439e0a14c5669af19d175b3d
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-09-20 12:10:22 +1200

    feat: basic modal card with more styling

--- client/app/components/generic/modal-window/ModalWindow.tsx
+++ client/app/components/generic/modal-window/ModalWindow.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import React, { useState } from "react";
+import { ArrowRightIcon } from "@heroicons/react/24/solid";
+
 
 const cards = [
   {
-    title: "About Us",
-    text: "Our portal brings together tools, services, and a supportive community to help you prioritise your wellbeing.",
+    title: "Awareness",
+    text: "Awareness is about noticing how you’re really feeling—your thoughts, emotions, and energy levels—so you can respond with care.",
   },
   {
-    title: "Wellbeing Quiz",
-    text: "Take our short quiz to check in, reflect, and discover tailored resources and support to boost your wellbeing today.",
+    title: "Communication",
+    text: "Communication helps you build stronger connections, express yourself clearly, and create supportive relationships that boost your wellbeing.",
   },
   {
-    title: "Events",
-    text: "Join workshops, socials and wellbeing weeks. Something for everyone!",
+    title: "Selfcare",
+    text: "Self-care means making time for the little things that recharge you—because looking after yourself is essential, not optional.",
   },
 ];
 
@@ -21,25 +23,29 @@ const ModalWindow = () => {
   const [selectedCard, setSelectedCard] = useState<null | typeof cards[0]>(null);
 
   return (
-    <div className="">
+    <div className="w-full flex justify-center">
       {/* always visible cards */}
       <div className="flex flex-col md:flex-row gap-4 md:gap-0 md:divide-x md:divide-gray-300">
         {cards.map((card, idx) => (
           <div
             key={idx}
             onClick={() => setSelectedCard(card)}
-            className="flex-1 p-6 bg-gray-200 cursor-pointer hover:bg-gray-300 transition"
+            className="flex-1 p-6 bg-[#3A7D8C] md:w-full w-[400px] justify-center align-items items-center cursor-pointer hover:bg-gray-300 transition "
           >
-            <h1 className="font-bold mb-2">{card.title}</h1>
+            <h1 className=" font-bold mb-2 flex items-center gap-4 ">
+              {card.title}
+              <ArrowRightIcon className="w-5 h-5 text-gray-600 text-white" />
+            </h1>
             <p>{card.text}</p>
           </div>
+
         ))}
       </div>
 
       {/* modal overlay */}
       {selectedCard && (
         <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white rounded-2xl shadow-lg max-w-md w-full p-6 relative">
+          <div className="bg-[#3A7D8C] rounded-2xl shadow-lg max-w-md w-full p-6 relative">
             {/* close button */}
             <button
               onClick={() => setSelectedCard(null)}
