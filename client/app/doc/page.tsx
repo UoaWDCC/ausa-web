c40eec8df5affcc124f5d7d09905eb964fa5a6a8
ZL <ray040406@gmail.com>
2025-09-21 16:07:37 +1200

    feat: storybook file

--- client/app/doc/page.tsx
+++ client/app/doc/page.tsx
@@ -1,13 +1,7 @@
 import React from "react"
-
-import ModalWindow from "./components/generic/modal-window/ModalWindow"
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center gap-10 bg-background">
-      {/* modal window test */}
-      <div className="">
-        <ModalWindow />
-      </div>
       {/* Headings */}
       <div className="flex flex-col items-center gap-6">
         <h1 className="h1" style={{ color: "black", marginBottom: "24px" }}>
