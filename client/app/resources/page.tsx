8e048a6180e08acbef3adbcf950ddc1d534d8f85
A.M. Zapata <annameepymoop@gmail.com>
2025-10-04 22:07:14 +1300

    add new resources page

---
+++ client/app/resources/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React from "react"
+import TextBox from "../components/composite/text-box/TextBox"
+import Dropdown from "../components/composite/dropdown/Dropdown"
+import Button from "../components/generic/button/regular/Button"
+
+const Resources = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default Resources
