532e11ed7a390cc56c6ae6db7ac6a4f68db2b3ad
VINCENT <vwal290@aucklanduni.ac.nz>
2025-10-04 15:54:03 +1300

    redo carousel card component to be more responsive

--- client/app/components/composite/carousel/CarouselCard.tsx
+++ client/app/components/composite/carousel/CarouselCard.tsx
@@ -6,13 +6,6 @@ interface CarouselCardProps {
   description: string
   image: string
   link: string
-  variant?: "wide" | "thin"
-}
-
-const getDimensions = (variant: "wide" | "thin") => {
-  if (variant === "wide") return { width: 630, height: 450 }
-  // default to thin: mobile-first design
-  return { width: 350, height: 470 }
 }
 
 const CarouselCard = ({
@@ -20,19 +13,20 @@ const CarouselCard = ({
   description,
   image,
   link,
-  variant,
 }: CarouselCardProps) => {
-  const { width, height } = getDimensions(variant || "thin")
   return (
     <div
-      className="relative rounded-lg overflow-hidden shadow-md bg-white flex flex-col border-gray-200 border"
-      style={{ width, height }}
+      className="
+        relative rounded-lg overflow-hidden shadow-md bg-white flex flex-col border border-gray-200
+        w-[350px] h-[470px]       // mobile
+        md:w-[630px] md:h-[450px] // desktop
+        shrink-0
+      "
     >
       <img src={image} alt={title} className="w-full h-1/2 object-cover" />
 
       <div className="p-4 flex flex-col flex-1">
         <h3 className="text-lg text-gray-600 font-semibold">{title}</h3>
-
         <p className="text-sm text-gray-600 mt-2 line-clamp-3">{description}</p>
 
         <div className="mt-auto mb-4">
