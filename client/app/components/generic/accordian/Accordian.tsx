500c5518c2d0d9b2a0f14172f4894ce6501bb605
Ashlee Shum <ashu443@aucklanduni.ac.nz>
2025-10-05 21:53:26 +1300

    feat: finished accordian component

--- client/app/components/generic/accordian/Accordian.tsx
+++ client/app/components/generic/accordian/Accordian.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+"use client";
 
-const Accordian = () => {
-  return <div>Accordian Component</div>;
+import React, { useState } from "react";
+import { ChevronDownIcon } from "@heroicons/react/24/solid";
+
+
+interface AccordianProps {
+  title: string;
+  content: string;
 }
 
+const Accordian: React.FC<AccordianProps> = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleAccordian = () => {
+    setIsOpen((prev) => !prev);
+  };
+
+
+  return (
+    <div className="text-black border-b border-[#043873]">
+      <button
+        onClick={() => setIsOpen(prev => !prev)}
+        className="flex justify-between items-center w-full px-4 py-5 cursor-pointer"
+      >
+        <span>{title}</span>
+
+        <ChevronDownIcon
+          className={`w-6 h-6 transition-transform duration-300 ${
+            isOpen ? "rotate-180" : "rotate-0"
+          }`}
+        />
+      </button>
+
+      {isOpen && <div className="px-4 py-3 text-gray-700">{content}</div>}
+    </div>
+  );
+};
+
 export default Accordian;
+
---
+++ client/app/faqs/page.tsx
@@ -0,0 +1,34 @@
+import Accordian from '../components/generic/accordian/Accordian';
+import React from 'react';
+
+const faqData = [
+  {
+    question: "What kinds of wellbeing support are available at UOA?",
+    answer: "Most universities offer a range of wellbeing support services to help students manage both personal and academic challenges. These can include free or low-cost counselling sessions, student support advisors, peer mentoring, wellbeing workshops, and spaces to connect with others. Many universities also provide academic accommodations if you’re experiencing difficulties that affect your studies. You can usually find details on your university’s student services or wellbeing page, or by reaching out directly to your student support centre"
+  },
+  {
+    question: "What kinds of wellbeing support are available at UOA?",
+    answer: "Most universities offer a range of wellbeing support services to help students manage both personal and academic challenges. These can include free or low-cost counselling sessions, student support advisors, peer mentoring, wellbeing workshops, and spaces to connect with others. Many universities also provide academic accommodations if you’re experiencing difficulties that affect your studies. You can usually find details on your university’s student services or wellbeing page, or by reaching out directly to your student support centre"
+  },
+  {
+    question: "What kinds of wellbeing support are available at UOA?",
+    answer: "Most universities offer a range of wellbeing support services to help students manage both personal and academic challenges. These can include free or low-cost counselling sessions, student support advisors, peer mentoring, wellbeing workshops, and spaces to connect with others. Many universities also provide academic accommodations if you’re experiencing difficulties that affect your studies. You can usually find details on your university’s student services or wellbeing page, or by reaching out directly to your student support centre"
+  }
+];
+
+const FAQ = () => {
+  return (
+    <div className="mt-40 px-4 max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold mb-4 text-black">Frequently Asked Questions</h1>
+      {/* divide-y will create a single line between each accordion */}
+      <div className="divide-y divide-[#043873]">
+        {faqData.map((faq, index) => (
+          <Accordian key={index} title={faq.question} content={faq.answer} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default FAQ;
+
