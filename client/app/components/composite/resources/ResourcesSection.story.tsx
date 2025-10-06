ea4ef3339bd99053d7ae615708c510fe7315f93b
A.M. Zapata <annameepymoop@gmail.com>
2025-10-06 22:26:25 +1300

    added resources section + some styling

--- client/app/components/composite/resources/ResourcesSection.story.tsx
+++ client/app/components/composite/resources/ResourcesSection.story.tsx
@@ -1,10 +1,35 @@
 import type { Meta, StoryObj } from "@storybook/nextjs"
 import ResourcesSection from "./ResourcesSection"
-import ResourcesLink from "./ResourcesLink"
 
 const meta: Meta<typeof ResourcesSection> = {
-  title: "Components/Contacts/ContactsLeft",
+  title: "Components/Resources/ResourcesSection",
   component: ResourcesSection,
   parameters: {
     layout: "fullscreen",
   },
+}
+
+export default meta
+type Story = StoryObj<typeof ResourcesSection>
+
+export const Default: Story = {
+  args: {
+    sectionTitle: "Mental Health Resources",
+    sectionDescription:
+      "Explore trusted resources and organizations that support student wellbeing.",
+    resources: [
+      {
+        title: "Lifeline New Zealand",
+        description:
+          "Free 24/7 helpline for anyone feeling distressed or in need of someone to talk to.",
+        url: "https://www.lifeline.org.nz/",
+      },
+      {
+        title: "Youthline",
+        description:
+          "A safe space for young people to get help with anything that’s worrying them.",
+        url: "https://www.youthline.co.nz/",
+      },
+    ],
+  },
+}
