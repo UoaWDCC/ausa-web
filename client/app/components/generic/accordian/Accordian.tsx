719d2a1b06f13c9494303a750efcc6f43cc19102
Gavin Ding <gavinzlding@gmail.com>
2025-10-08 12:26:36 +1300

    fix: lint

--- client/app/components/generic/accordian/Accordian.tsx
+++ client/app/components/generic/accordian/Accordian.tsx
@@ -1,26 +1,20 @@
-"use client";
+"use client"
 
-import React, { useState } from "react";
-import { ChevronDownIcon } from "@heroicons/react/24/solid";
-
+import React, { useState } from "react"
+import { ChevronDownIcon } from "@heroicons/react/24/solid"
 
 interface AccordianProps {
-  title: string;
-  content: string;
+  title: string
+  content: string
 }
 
 const Accordian: React.FC<AccordianProps> = ({ title, content }) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleAccordian = () => {
-    setIsOpen((prev) => !prev);
-  };
-
+  const [isOpen, setIsOpen] = useState(false)
 
   return (
     <div className="text-black border-b border-[#043873]">
       <button
-        onClick={() => setIsOpen(prev => !prev)}
+        onClick={() => setIsOpen((prev) => !prev)}
         className="flex justify-between items-center w-full px-4 py-5 cursor-pointer"
       >
         <span>{title}</span>
@@ -34,8 +28,7 @@ const Accordian: React.FC<AccordianProps> = ({ title, content }) => {
 
       {isOpen && <div className="px-4 py-3 text-gray-700">{content}</div>}
     </div>
-  );
-};
+  )
+}
 
-export default Accordian;
-
+export default Accordian
