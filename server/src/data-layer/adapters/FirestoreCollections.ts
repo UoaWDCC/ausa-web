212d1851f3aa0d99a9c5e4850f9b8599dd26779d
VINCENT <vwal290@aucklanduni.ac.nz>
2025-10-14 01:12:31 +1300

    feat: add resources collection to Firestore adapter

--- server/src/data-layer/adapters/FirestoreCollections.ts
+++ server/src/data-layer/adapters/FirestoreCollections.ts
@@ -1,9 +1,11 @@
 import type { User } from "../../business-layer/models/User"
-import { USERS_COLLECTION } from "./CollectionNames"
+import type { Resource } from "../../business-layer/models/Resource"
+import { RESOURCES_COLLECTION, USERS_COLLECTION } from "./CollectionNames"
 import firestore from "./Firestore"
 
 const FirestoreCollections = {
   users: firestore.collection<User>(USERS_COLLECTION),
+  resources: firestore.collection<Resource>(RESOURCES_COLLECTION),
 } as const
 
 export default FirestoreCollections
