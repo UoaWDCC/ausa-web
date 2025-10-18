9511d546b4626a23e88287b6d8b0f5e6ae2a8590
zl <ray040406@gmail.com>
2025-10-18 18:31:31 +1300

    feat: color change 404

--- client/app/not-found.tsx
+++ client/app/not-found.tsx
@@ -2,7 +2,7 @@ import Page from "@/public/page-not-found-svgrepo-com.svg"
 import Image from "next/image"
 const NotFound = () => {
   return (
-    <div className="w-full min-h-[100vh] flex flex-col justify-center items-center gap-4">
+    <div className="w-full bg-[#faf7f2] min-h-[100vh] flex flex-col justify-center items-center gap-4">
       <Image src={Page} alt="404 page not found" width={300} height={300} />
       <h3 className="h3 text-black">404</h3>
       <p className="h5 text-black m-4">{`The page you're looking for does not exist :(`}</p>
