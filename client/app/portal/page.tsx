9fad9a804fa808ccc879a0ea1fa65252b52b3658
zl <ray040406@gmail.com>
2025-10-18 19:37:28 +1300

    feat: styling, structure

--- client/app/portal/page.tsx
+++ client/app/portal/page.tsx
@@ -8,30 +8,30 @@ import Link from "next/link"
 const PortalPage = () => {
   return (
     <Background>
-      <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-accent-warm-400 via-[#FAF7F2] to-white ">
+      <main className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-b from-[#043873] bg-[#faf7f2]">
         {/* <PortalCard> */}
-          <h2 className="text-2xl italic mb-6 text-center text-black">
+          <h2 className="text-2xl italic mb-6 text-center text-[#FAF7F2]">
             Welcome to AUSA Wellbeing
           </h2>
           <div className="grid grid-cols-2 w-[400px] gap-4">
             <Link href="/">
-              <div className="border bg-primary rounded-lg w-48 h-48 text-center text-sm bg-gradient-to-r text-white">
-                Home
+              <div className="bg-[#FFDE91] rounded-lg w-48 h-48 justify-center items-center flex flex-col text-sm text-white shadow-lg">
+                <h4 className="font-bold text-2xl text-black">Home</h4>
               </div>
             </Link>
             <Link href="/quiz">
-              <div className="border bg-primary rounded-lg w-48 h-48 text-center text-sm bg-gradient-to-r text-white">
-                Quiz
+              <div className="bg-[#FFDE91] rounded-lg w-48 h-48 justify-center items-center flex flex-col text-sm bg-gradient-to-r text-white shadow-lg">
+                <h4 className="font-bold text-2xl text-black">Quiz</h4>
               </div>
             </Link>
             <Link href="/contact">
-              <div className="border bg-primary rounded-lg w-48 h-48 text-center text-sm bg-gradient-to-r text-white">
-                Contact
+              <div className="bg-[#FFDE91] rounded-lg w-48 h-48 justify-center items-center flex flex-col text-sm bg-gradient-to-r text-white shadow-lg">
+                <h4 className="font-bold text-2xl text-black">Contact</h4>
               </div>
             </Link>
              <Link href="/signup">
-              <div className="border bg-primary rounded-lg w-48 h-48 text-center text-sm bg-gradient-to-r text-white">
-                Sign Up
+              <div className="bg-[#FFDE91] rounded-lg w-48 h-48 justify-center items-center flex flex-col text-sm bg-gradient-to-r text-white shadow-lg">
+                <h4 className="font-bold text-2xl text-black">Signup</h4>
               </div>
             </Link>
           </div>
