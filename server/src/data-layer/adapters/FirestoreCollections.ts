dc4ad908dce383136eb0eb744fea5280b384bda9
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-10-19 19:18:08 +1300

    feat: adds resource models and collections (#35)
    * feat: adds resource models and collections
    
    * feat(data-layer): add resource data repo
    
    * refactor(data-layer): use single collection for resources
    
    * build(package): add `generate:types` script
    
    * feat(controllers): add resource controller
    
    * feat(frontend): add resource service and types
    
    * feat(services): add resource queries
    
    * chore(prettier): ignore frontend generated schema and remove unused
    
    * chore: fix typo

--- server/src/data-layer/adapters/FirestoreCollections.ts
+++ server/src/data-layer/adapters/FirestoreCollections.ts
@@ -1,10 +1,16 @@
+import { BaseResource } from "business-layer/models/Resource"
 import type { User } from "../../business-layer/models/User"
-import { QUIZ_COLLECTION, USERS_COLLECTION } from "./CollectionNames"
+import {
+  QUIZ_COLLECTION,
+  RESOURCES_COLLECTION,
+  USERS_COLLECTION,
+} from "./CollectionNames"
 import firestore from "./Firestore"
 
 const FirestoreCollections = {
   users: firestore.collection<User>(USERS_COLLECTION),
   quizzes: firestore.collection(QUIZ_COLLECTION),
+  resources: firestore.collection<BaseResource>(RESOURCES_COLLECTION),
 } as const
 
 export default FirestoreCollections
