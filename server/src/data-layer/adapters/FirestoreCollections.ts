140bf9762c05ebdd3c7689ff2cad321703318a85
Jeffery <61447509+jeffplays2005@users.noreply.github.com>
2025-10-19 18:34:11 +1300

    feat: adds resource models and collections

--- server/src/data-layer/adapters/FirestoreCollections.ts
+++ server/src/data-layer/adapters/FirestoreCollections.ts
@@ -1,10 +1,30 @@
+import { BaseResource } from "business-layer/models/Resource"
 import type { User } from "../../business-layer/models/User"
-import { QUIZ_COLLECTION, USERS_COLLECTION } from "./CollectionNames"
+import {
+  ACADEMIC_RESOURCES_COLLECTION,
+  EXTERNAL_RESOURCES_COLLECTION,
+  FINANCIAL_RESOURCES_COLLECTION,
+  QUIZ_COLLECTION,
+  USERS_COLLECTION,
+  WELLBEING_RESOURCES_COLLECTION,
+} from "./CollectionNames"
 import firestore from "./Firestore"
 
 const FirestoreCollections = {
   users: firestore.collection<User>(USERS_COLLECTION),
   quizzes: firestore.collection(QUIZ_COLLECTION),
+  financialResources: firestore.collection<BaseResource>(
+    FINANCIAL_RESOURCES_COLLECTION,
+  ),
+  academicResources: firestore.collection<BaseResource>(
+    ACADEMIC_RESOURCES_COLLECTION,
+  ),
+  externalResources: firestore.collection<BaseResource>(
+    EXTERNAL_RESOURCES_COLLECTION,
+  ),
+  wellbeingResources: firestore.collection<BaseResource>(
+    WELLBEING_RESOURCES_COLLECTION,
+  ),
 } as const
 
 export default FirestoreCollections
